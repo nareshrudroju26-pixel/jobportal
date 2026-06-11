bb862cfa0bdce85998965fba4eb9545397ff2bbe
Naresh Rudroju <nareshrudroju26@gmail.com>
2026-06-11 20:02:42 +0530

    Initial commit

--- src/main/java/com/eazybytes/jobportal/security/PathsConfig.java
+++ src/main/java/com/eazybytes/jobportal/security/PathsConfig.java
@@ -23,7 +23,9 @@ public class PathsConfig {
                 "/swagger-resources/**",
                 "/swagger-ui.html",
                 "/webjars/**",
-                "/jobportal/actuator/**"
+                "/jobportal/actuator/**",
+                "/api/todos/**",
+                "/api/posts/**"
         );
     }
 
